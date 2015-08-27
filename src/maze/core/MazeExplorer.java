629b23665930b072504662834f70580f0e717458
nguyenmv2 <nguyenm2@hendrix.edu>
2015-08-26 22:34:12 -0500

    Test 1 passed + Test 2 still failing

---
+++ src/maze/core/MazeExplorer.java
@@ -0,0 +1,72 @@
+package maze.core;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.TreeSet;
+
+import search.core.BestFirstObject;
+
+public class MazeExplorer implements BestFirstObject<MazeExplorer> {
+	private Maze m;
+	private MazeCell location;
+	private TreeSet<MazeCell> treasureFound; 
+	
+	public MazeExplorer(Maze m, MazeCell location) {
+		this.m = m;
+		this.location = location;
+		treasureFound = new TreeSet<MazeCell>();
+	}
+	
+	public MazeCell getLocation() {return location;}
+
+
+
+	@Override
+	public ArrayList<MazeExplorer> getSuccessors() {
+		ArrayList<MazeExplorer> result = new ArrayList<MazeExplorer>();
+		// TODO: It should add as a successor every adjacent, unblocked neighbor square.
+        ArrayList<MazeCell> neighbors = m.getNeighbors(this.getLocation());
+
+        for ( MazeCell node : neighbors){
+            if ( !m.blocked(this.getLocation(), node)) {
+                result.add(new MazeExplorer(this.m, node));
+                if (m.isTreasure(node)){ treasureFound.add(node);}
+
+            }
+        }
+        for (MazeExplorer node : result){
+           node.addTreasures(treasureFound);
+        }
+        return result;
+	}
+	 public void addTreasures(Collection<MazeCell> treasures) {
+		treasureFound.addAll(treasures);
+	}
+	
+	public String toString() {
+		StringBuilder treasures = new StringBuilder();
+		for (MazeCell t: treasureFound) {
+			treasures.append(";");
+			treasures.append(t.toString());
+		}
+		return "@" + location.toString() + treasures.toString();
+	}
+	
+	@Override
+	public int hashCode() {return toString().hashCode();}
+	
+	@Override
+	public boolean equals(Object other) {
+		if (other instanceof MazeExplorer) {
+			return achieves((MazeExplorer)other);
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public boolean achieves(MazeExplorer goal) {
+		return this.location.equals(goal.location) && this.treasureFound.equals(goal.treasureFound);
+	}
+
+}
