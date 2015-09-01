0cc910fda9b3f64ffd8293382180a77121ebadde
nguyenmv2 <nguyenm2@hendrix.edu>
2015-09-01 10:23:49 -0500

    Update files

--- src/maze/heuristics/GreedyTreasure.java
+++ src/maze/heuristics/GreedyTreasure.java
@@ -4,30 +4,22 @@ import maze.core.MazeCell;
 import maze.core.MazeExplorer;
 import search.core.BestFirstHeuristic;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.SortedSet;
-
 /**
  * Created by nguye on 9/1/2015.
  */
 public class GreedyTreasure implements BestFirstHeuristic<MazeExplorer> {
-
     @Override
     public int getDistance(MazeExplorer node, MazeExplorer goal) {
-        SortedSet<MazeCell> treasureList = node.getTreasureFound();
-        if (treasureList.size() == 0 ){
-            return Math.abs(node.getLocation().X() - goal.getLocation().X()) + Math.abs(node.getLocation().Y() - goal.getLocation().Y()) +
-                            Math.abs(node.getLocation().X() -goal.getLocation().X()) + Math.abs(node.getLocation().Y() -goal.getLocation().Y());
+        int minDistToTreasure = Integer.MAX_VALUE;
+        for ( MazeCell treasure : node.getTreasureFound()){
+            int distToNode = getManhantanDistance(treasure, node.getLocation());
+            if (  distToNode < minDistToTreasure ){
+                minDistToTreasure = distToNode;
+            }
         }
-        ArrayList<Integer> list = new ArrayList<>();
-        for (MazeCell n : treasureList){
-            list.add(
-                    Math.abs(n.X() - goal.getLocation().X()) + Math.abs(n.Y() - goal.getLocation().Y()) +
-                            Math.abs(n.X() - node.getLocation().X()) + Math.abs(n.Y() - node.getLocation().Y())
-                    );
-        }
-        Collections.sort(list);
-        return list.get(0);
+        return minDistToTreasure;
+    }
+    public int getManhantanDistance(MazeCell first, MazeCell second){
+        return Math.abs(first.X() - second.X()) + Math.abs(second.Y() - first.Y());
     }
 }
---
+++ src/maze/heuristics/ManhattanTreasure.java
@@ -0,0 +1,33 @@
+package maze.heuristics;
+
+import maze.core.MazeCell;
+import maze.core.MazeExplorer;
+import search.core.BestFirstHeuristic;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.SortedSet;
+
+/**
+ * Created by nguye on 9/1/2015.
+ */
+public class ManhattanTreasure implements BestFirstHeuristic<MazeExplorer> {
+
+    @Override
+    public int getDistance(MazeExplorer node, MazeExplorer goal) {
+        SortedSet<MazeCell> treasureList = node.getTreasureFound();
+        if (treasureList.size() == 0 ){
+            return Math.abs(node.getLocation().X() - goal.getLocation().X()) + Math.abs(node.getLocation().Y() - goal.getLocation().Y()) +
+                            Math.abs(node.getLocation().X() -goal.getLocation().X()) + Math.abs(node.getLocation().Y() -goal.getLocation().Y());
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        for (MazeCell n : treasureList){
+            list.add(
+                    Math.abs(n.X() - goal.getLocation().X()) + Math.abs(n.Y() - goal.getLocation().Y()) +
+                            Math.abs(n.X() - node.getLocation().X()) + Math.abs(n.Y() - node.getLocation().Y())
+                    );
+        }
+        Collections.sort(list);
+        return list.get(0);
+    }
+}
